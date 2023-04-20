da7640cd322cd51847bb1c23f2ae0914e4625079
rbb <rick1stl@gmail.com>
2023-04-20 07:42:05 -0500

    loop thru all 3 in one cmd

--- src/index.mjs
+++ src/index.mjs
@@ -8,68 +8,66 @@ import {
 } from "node:fs";
 import puppeteer from "puppeteer";
 
-const person = process.env.PERSON || "unknown";
+const persons = ["rbb", "blb", "alex"];
 const port = 8081;
-const srcMediaDir = `./wwwroot/${person}SrcMedia`;
-const destMediaDir = `./${person}NewMedia`;
 
 async function start() {
-  console.log("-----d--d-d-d-d-d-d-d-d");
-  console.log(srcMediaDir);
-  console.log(destMediaDir);
+  // console.time("DO_IT");
 
-  console.time("DO_IT");
+  for await (const person of persons) {
+    const srcMediaDir = `./wwwroot/${person}SrcMedia`;
+    const destMediaDir = `./${person}NewMedia`;
 
-  if (existsSync(destMediaDir)) rmdirSync(destMediaDir, { recursive: true });
-  mkdirSync(destMediaDir);
+    if (existsSync(destMediaDir)) rmdirSync(destMediaDir, { recursive: true });
+    mkdirSync(destMediaDir);
 
-  const allSrcFiles = readdirSync(srcMediaDir);
+    const srcFiles = readdirSync(srcMediaDir);
 
-  const nonJpgFiles = allSrcFiles.filter((file) => {
-    const name = file.toLowerCase();
-    if (name.endsWith(".jpg") || name.endsWith(".jpeg")) return false;
-    return true;
-  });
-
-  nonJpgFiles.forEach((file) => {
-    const fileData = statSync(`${srcMediaDir}/${file}`);
-    copyFileSync(
-      `${srcMediaDir}/${file}`,
-      `${destMediaDir}/${fileData.mtime.getFullYear()}-${file}`
-    );
-  });
-
-  const jpgFiles = allSrcFiles
-    .filter((file) => {
+    const nonJpgFiles = srcFiles.filter((file) => {
       const name = file.toLowerCase();
-      if (name.endsWith(".jpg") || name.endsWith(".jpeg")) return true;
-      return false;
-    })
-    .map((file) => {
-      const fileData = statSync(`${srcMediaDir}/${file}`);
-      return { name: file, year: fileData.mtime.getFullYear() };
+      if (name.endsWith(".jpg") || name.endsWith(".jpeg")) return false;
+      return true;
     });
 
-  for await (const file of jpgFiles) {
-    const browser = await puppeteer.launch();
-    // const browser = await puppeteer.launch({ headless: false, devtools: true });
-    // const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 333, height: 333 });
-    await page.goto(
-      `http://localhost:${port}?fileName=${file.name}&year=${file.year}&person=${person}`
-    );
-    await new Promise((resolve) => setTimeout(resolve, 99));
-    await page.screenshot({
-      path: `${destMediaDir}/${file.year}-${file.name}`,
-      fullPage: true,
-      type: "jpeg",
-      quality: 90,
+    nonJpgFiles.forEach((file) => {
+      const fileData = statSync(`${srcMediaDir}/${file}`);
+      copyFileSync(
+        `${srcMediaDir}/${file}`,
+        `${destMediaDir}/${fileData.mtime.getFullYear()}-${file}`
+      );
     });
-    await browser.close();
+
+    const jpgFiles = srcFiles
+      .filter((file) => {
+        const name = file.toLowerCase();
+        if (name.endsWith(".jpg") || name.endsWith(".jpeg")) return true;
+        return false;
+      })
+      .map((file) => {
+        const fileData = statSync(`${srcMediaDir}/${file}`);
+        return { name: file, year: fileData.mtime.getFullYear() };
+      });
+
+    for await (const file of jpgFiles) {
+      const browser = await puppeteer.launch();
+      // const browser = await puppeteer.launch({ headless: false, devtools: true });
+      const page = await browser.newPage();
+      await page.setViewport({ width: 333, height: 333 });
+      await page.goto(
+        `http://localhost:${port}?fileName=${file.name}&year=${file.year}&person=${person}`
+      );
+      await new Promise((resolve) => setTimeout(resolve, 99));
+      await page.screenshot({
+        path: `${destMediaDir}/${file.year}-${file.name}`,
+        fullPage: true,
+        type: "jpeg",
+        quality: 90,
+      });
+      await browser.close();
+    }
   }
 
-  console.timeEnd("DO_IT");
+  // console.timeEnd("DO_IT");
 }
 
 start();
